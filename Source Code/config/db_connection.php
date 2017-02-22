241d9c251ce7ad1c5fdf6a2dfd52087bde28c772
Jola Oke <jola.oke16@gmail.com>
2017-02-22 11:06:47 -0500

    Add files via upload

---
+++ Source Code/config/db_connection.php
@@ -0,0 +1,6 @@
+<?php
+$username="root";
+$password="";
+$database="fitness_pal";
+$conn=mysqli_connect('localhost',$username,$password,$database) or die("Connection could not be established.");
+?>
